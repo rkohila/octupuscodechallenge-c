45f6f3f76aad96b2b793595bcec49edf38e0fd0b
kohila <rkohila@gmail.com>
2017-07-09 23:10:28 +0100

    first commit

---
+++ octupuscodechallenge/OurPeopleSearchPage.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+
+namespace octupuscodechallenge
+{
+	public class OurPeopleSearchPage
+	{
+
+		private IWebDriver driver;
+		private WebDriverWait webDriverWait;
+
+		public OurPeopleSearchPage(IWebDriver driver)
+		{
+			this.driver = driver;
+			webDriverWait = new WebDriverWait(driver,TimeSpan.FromSeconds(30));
+		}
+
+
+
+		private void focusOnSearchContainer()
+		{
+			IWebElement element = driver.FindElement(By.CssSelector(".search-container"));
+			Actions searchContainerAction = new Actions(driver);
+			searchContainerAction.MoveToElement(element);
+			searchContainerAction.Perform();
+		}
+
+		public void acceptCookieModal()
+		{
+			Boolean displayed = driver.FindElement(By.CssSelector(".modal-dialog>.modal-content")).Displayed;
+
+			IWebElement element = driver.FindElement(By.CssSelector(".modal-footer>a"));
+
+			Actions actions = new Actions(driver);
+			actions.MoveToElement(element);
+			//	webDriverWait.Until(ExpectedConditions.visibilityOf(element));
+			actions.Click();
+			actions.Build().Perform();
+		}
+
+
+		public void basicSearch(String searchText)
+		{
+
+			focusOnSearchContainer();
+
+			webDriverWait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".inputOne")));
+			IWebElement searchElement = driver.FindElement(By.CssSelector(".inputOne"));
+
+			waitForElement();
+			System.Diagnostics.Debug.WriteLine("waiting heere to type the value in texg box");
+
+			Actions actions = new Actions(driver);
+			actions.MoveToElement(searchElement);
+			actions.Click();
+			actions.SendKeys(searchText);
+			actions.Build().Perform();
+			System.Diagnostics.Debug.WriteLine("hope its typed ");
+		}
+
+		private void waitForElement()
+		{
+			Thread.Sleep(9000);
+		}
+
+		public List<String> searchResults()
+		{
+			webDriverWait.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.CssSelector(".row.searchResults.effect-3")));
+
+			IList<IWebElement> searchResultNames = driver.FindElements(By.CssSelector(".row.searchResults.effect-3>a>div>h2"));
+			List<String> searchNameList = new List<String>();
+
+			foreach (IWebElement searchResult in searchResultNames)
+			{
+				System.Diagnostics.Debug.WriteLine("searchResult.getText() = " + searchResult.Text);
+				searchNameList.Add(searchResult.Text.ToLower());
+			}
+			return searchNameList;
+
+		}
+
+		public List<String> searchResultTeamNames()
+		{
+			IList<IWebElement> searchResultTeamNameList = driver.FindElements(By.CssSelector(".row.searchResults.effect-3>a>div>p"));
+			List<String> searchNameList = new List<String>();
+
+			foreach (IWebElement searchResult in searchResultTeamNameList)
+			{
+				searchNameList.Add(searchResult.Text.ToLower());
+			}
+			return searchNameList;
+
+		}
+
+		public void selectSearchResult()
+		{
+			IList<IWebElement> searchResultNames = driver.FindElements(By.CssSelector(".row.searchResults.effect-3"));
+			IWebElement webElement = searchResultNames[0];
+			webElement.Click();
+			waitForElement();
+		}
+
+		public void sortBy(String order)
+		{
+			IWebElement selectElement = driver.FindElement(By.CssSelector(".selectOne>div>select"));
+			SelectElement select = new SelectElement(selectElement);
+
+			if (order.Equals("desc"))
+			{
+				select.SelectByValue("Z-A");
+			}
+
+		}
+
+
+		public Boolean isPostCodeDisplayed()
+		{
+			//webDriverWait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".inputTwo"))));
+			return driver.FindElement(By.CssSelector(".inputTwo")).Displayed;
+		}
+
+	public void searchByPostcode(String postCode)
+	{
+		IWebElement element = driver.FindElement(By.CssSelector(".inputTwo"));
+		Actions actions = new Actions(driver);
+		actions.MoveToElement(element);
+		actions.Click();
+		actions.SendKeys(postCode);
+		actions.Build().Perform();
+	}
+
+public void selectATeam(String teamName)
+{
+	focusOnSearchContainer();
+	IWebElement selectElement = driver.FindElement(By.CssSelector(".selectTwo>div>select"));
+SelectElement select = new SelectElement(selectElement);
+			select.SelectByValue(teamName);
+
+}
+
+public String getCurrentUrl()
+{
+			return driver.Url;   }
+}
+
+}
---
+++ octupuscodechallenge/ourPeopleSearch.cs
@@ -0,0 +1,109 @@
+﻿using NUnit.Framework;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using System.Collections.Generic;
+
+
+
+namespace octupuscodechallenge
+{
+	[TestFixture()]
+	public class Test
+	{
+		private IWebDriver driver;
+		private String URL = "https://octopusinvestments.com/adviser/about-us/our-people";
+
+		[SetUp]
+		public void SetupTest() {  
+			driver = new ChromeDriver();
+
+		}
+
+		[TearDown]
+		public void TearDownTest()
+		{
+			driver.Quit();
+		}
+
+
+	
+		[Test()]
+		public void ShouldSearchByName()
+		{
+			driver.Navigate().GoToUrl(URL);
+			OurPeopleSearchPage ourPeopleSearchPage = new OurPeopleSearchPage(driver);
+			PageFactory.InitElements(driver, ourPeopleSearchPage);
+			ourPeopleSearchPage.acceptCookieModal();
+            waitForPageLoad(driver);
+			ourPeopleSearchPage.basicSearch("Adam");
+			List<String> nameList = ourPeopleSearchPage.searchResults();
+			Assert.IsTrue(validateNameList(nameList, "adam"));
+			//ourPeopleSearchPage.selectSearchResult();
+			//System.Diagnostics.Debug.WriteLine("current url ", ourPeopleSearchPage.getCurrentUrl());
+            //Assert.IsTrue(ourPeopleSearchPage.getCurrentUrl().Contains("adam"));
+			
+		}
+
+		[Test()]
+		public void shouldSearchTextSortByDescAndVerifyTheResult()
+		{
+			driver.Navigate().GoToUrl(URL);
+			OurPeopleSearchPage ourPeopleSearchPage = new OurPeopleSearchPage(driver);
+			PageFactory.InitElements(driver, ourPeopleSearchPage);
+			ourPeopleSearchPage.acceptCookieModal();
+            waitForPageLoad(driver);
+			ourPeopleSearchPage.basicSearch("ab");
+			ourPeopleSearchPage.sortBy("desc");
+			List<String> nameList = ourPeopleSearchPage.searchResults();
+
+			Assert.IsTrue(validateNameList(nameList, "ab"));
+
+
+		}
+
+        [Test()]
+        public void shouldSelectBDTAndVerifyTheResults(){
+            String team = "Business development team";
+			driver.Navigate().GoToUrl(URL);
+			OurPeopleSearchPage ourPeopleSearchPage = new OurPeopleSearchPage(driver);
+			PageFactory.InitElements(driver, ourPeopleSearchPage);
+			ourPeopleSearchPage.acceptCookieModal();
+			waitForPageLoad(driver);
+			ourPeopleSearchPage.selectATeam(team);
+            Assert.IsTrue(ourPeopleSearchPage.isPostCodeDisplayed());
+
+			List<String> searchResultTeamName = ourPeopleSearchPage.searchResultTeamNames();
+			Assert.IsTrue(validateNameList(searchResultTeamName, team));
+
+        }
+
+		private Boolean validateNameList(List<String> nameList, String text)
+		{
+			Boolean retValue = true;
+			foreach (String name in nameList)
+			{
+			
+				System.Diagnostics.Debug.WriteLine("searchResult name iin validate name list () = "+ name);
+				if (!name.ToLower().Contains(text.ToLower()))
+				{
+					retValue = false;
+				}
+				System.Diagnostics.Debug.WriteLine("searchResult.getText() = " + retValue);
+			}
+			return retValue;
+		}
+
+		static void waitForPageLoad(IWebDriver driver)
+		{
+			WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+
+			    wait.Until((wdriver) => (driver as IJavaScriptExecutor).ExecuteScript("return document.readyState").Equals("complete"));
+
+
+
+		    }
+	}
+}
